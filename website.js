05f50f450fc5a94806d622913f16fd672c799b0e
annupriya731 <162053101+annupriya731@users.noreply.github.com>
2024-05-24 23:19:00 +0530

    .
    ..............

---
+++ website.js
@@ -0,0 +1,16 @@
+
+let top = document.querySelectorAll('.topdiv');
+
+let links = document.querySelectorAll('nav a');
+window.onscroll = ( =>{
+
+
+    sec.forEach(topdiv=>{
+        let top= window.scrollY;
+        let offset = topdiv.offsetTop;
+        let height = topdiv.offsetheight;
+        let id= topdiv.getattribute('id')
+
+    })
+})
+
